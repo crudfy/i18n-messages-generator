2f4cc1ba14b3ba8f0e2e26a2767336a264771e2b
Dell XPS <seidya.dev.mhb@gmail.com>
2021-08-14 15:38:10 +0000

    Firt commit

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,38 @@
-export interface IExample {
-  hello(): string;
+export interface IData {
+  [key: string]: any
 }
+function addFileTranslations(parts: any, langs: any, data: any) {
+  parts.forEach((key: string, i: number) => {
+    if (!langs[key]) {
+      langs[key] = {}
+      if (parts.length === 1) {
+        langs[key] = data
+      }
+    }
+    parts.splice(i, 1)
+    addFileTranslations(parts, langs[key], data)
+  })
+}
+export function buildLangFile(requireLangFile: any) {
+  const langs: IData = {}
+  requireLangFile
+      .keys()
+      .filter((it: string) => {
+        // @ts-ignore
+        return ['ts', 'js'].includes(it.split('.').reverse()[0])
+      })
+      .map((fileName: string) => {
+        const langFile = requireLangFile(fileName).default || {}
+        const filename: string = fileName.replace('./', '')
+        const filenameParts: any = filename.split('/').filter((it) => {
+          return it !== 'index.ts' && it !== 'index.js'
+        }).map(it => {
+          return it.replace('.js', '').replace('.ts', '')
+        })
 
-export default class Example implements IExample {
-  public hello() {
-    return "World";
-  }
+        addFileTranslations(filenameParts, langs, langFile)
+
+      })
+
+  return langs
 }
